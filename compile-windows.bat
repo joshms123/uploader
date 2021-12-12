31d526405be4a88f90f4af693afdf7e2132b119f
masonasons <darkflier01@gmail.com>
2021-12-11 19:22:21 -0600

    update compile script

--- compile-windows.bat
+++ compile-windows.bat
@@ -1,5 +1,3 @@
-c:\python27\scripts\pyinstaller --clean --win-private-assemblies -F -w uploader.py
+pyinstaller --clean --win-private-assemblies -F -w uploader.py
 xcopy oggenc2.exe dist\oggenc2.exe
-xcopy app.defaults dist\app.defaults
-
 pause
